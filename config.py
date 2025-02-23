5fd86377afc9dd3021fc2d3a32016deca986d940
patriotic07 <duniyatg0@gmail.com>
2025-02-24 02:57:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "25513009"))
+API_HASH = getenv("API_HASH", "33ace9b0779df4387a8ad8b7fbe6b050")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION = getenv("SESSION", "")
