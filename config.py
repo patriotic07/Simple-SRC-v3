360cda8619877469bd5ffe19855136ef2f5aa77c
patriotic07 <duniyatg0@gmail.com>
2025-02-24 12:50:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ API_ID = int(getenv("API_ID", "25513009"))
 API_HASH = getenv("API_HASH", "33ace9b0779df4387a8ad8b7fbe6b050")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION = getenv("SESSION", "")
+OWNER_ID = int(getenv("OWNER_ID", "7645935938"))
+PRIVATE_CHANNEL = "-1002396692956" 
+FORCE_SUBS_CHANNEL = "saverestricted007"
